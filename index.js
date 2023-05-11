91b1be6da726b14a4516e1734f9ccbbe2e289038
rajata <rajata@amdocs.com>
2023-05-11 16:42:32 +0530

    Create main menu

---
+++ index.js
@@ -0,0 +1,31 @@
+const readLine = require("readline-sync");
+
+const main = () => {
+  console.log("*****************MAIN MENU************************");
+  console.log("1. View books");
+  console.log("2. Borrow a book");
+  console.log("3. Borrow a copy");
+  console.log("4. Return a book");
+
+  const option = Number(readLine.question("Choose an option from above menu:"));
+
+  switch (option) {
+    case 1: {
+      return;
+    }
+    case 2: {
+      return;
+    }
+    case 3: {
+      return;
+    }
+    case 4: {
+      return;
+    }
+    default: {
+      console.log("Wrong choice");
+    }
+  }
+};
+
+main();
